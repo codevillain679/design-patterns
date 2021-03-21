bef625ece5b863bd761a38016e0a4892167211fa
Niels Korporaal <napkorporaal@icloud.com>
2021-03-21 18:06:23 +0100

    :memo: Edited the header comments on different patterns

--- src/com/codevillain679/Main.java
+++ src/com/codevillain679/Main.java
@@ -1,13 +1,15 @@
 package com.codevillain679;
 
+import com.codevillain679.behavioral.chainofresponsibility.ChainOfResponsibilityPattern;
+import com.codevillain679.behavioral.memento.MementoPattern;
 import com.codevillain679.behavioral.state.StatePattern;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-       // new MementoPattern();
-
-        new StatePattern();
+//        new MementoPattern();
+//        new StatePattern();
+        new ChainOfResponsibilityPattern();
     }
 }
